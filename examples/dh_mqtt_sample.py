daa59d747977b22f721e48141e98f1cfebff3fbb
Mihnea Moldovan <mihnea.moldovan@gmail.com>
2015-06-26 18:17:16 +0300

    Add actuator example

--- examples/dh_mqtt_sample.py
+++ examples/dh_mqtt_sample.py
@@ -6,7 +6,7 @@ from time import sleep
 import json
 import mraa
 
-led_x = mraa.Gpio(13) // onboard led
+led_x = mraa.Gpio(13) # onboard led
 led_x.dir(mraa.DIR_OUT)
 
 PROJECT_ID = '1234'
@@ -16,7 +16,7 @@ API_KEY = '1234567-5131-407f-b76f-5158fa1234567'
 act_state = 0
 
 def on_led(client, userdata, message):
-	msg = str(message.payload)
+    msg = str(message.payload)
     j = json.loads(msg)
     global act_state
     act_state = int(j['state'])
