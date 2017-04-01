b38ca53681f2a99a28c2fb39f2bccd4f39150a1e
Ionut Cotoi <ionut@devicehub.net>
2017-04-01 14:49:40 +0300

    Bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ __author__ = 'Ionut Cotoi'
 from distutils.core import setup
 
 setup(name='devicehub',
-      version='0.8.8',
+      version='0.8.9',
       package_dir={'devicehub': 'devicehub'},
       packages=['devicehub'],
       install_requires=['httplib2', 'paho-mqtt', 'urllib3'],
